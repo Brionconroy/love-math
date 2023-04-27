14c73525273023e91014cc9b059797a86bbd80c1
Brionconroy <>
2023-04-27 15:24:37 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 alart("You clicked sub");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alart(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 
 });
 
@@ -20,9 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
  * the main game "Loop", when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
+
+    //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnwser() {
@@ -41,8 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
