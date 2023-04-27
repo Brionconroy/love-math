8c0dd5710c3f4bac3e11ff31df0e7d1db4c51920
Brionconroy <>
2023-04-27 11:52:56 +0000

    Add enemt listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alart("You clicked sub");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alart(`you clicked ${gameType}`);
+            }
+        });
+    }
+
+});
+
 function runGame() {
 
 }
